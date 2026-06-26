039a2e34cb5bc6f6114d1483d62640911b7fdddd
pierrefardel <81689742+pierrefardel@users.noreply.github.com>
2026-06-26 16:25:33 +0200

    kronolith dynamic css refactor

--- horde/info.php
+++ horde/info.php
@@ -1,2 +1,5 @@
 <?php
-$theme_name = _("Default New");
+$theme_name = _("UPJV");
+/* Thème autonome : ne charge PAS le CSS du thème default en fallback.
+   Lu par Horde_Themes_Cache (patch _inheritsDefault). */
+$theme_inherits = false;
